201c0df83a452fae64acf1ddeece2ed2b0e55b96
Mike Talley <miketalley@hotmail.com>
2019-02-10 17:53:44 -0500

    added keyboard shortcut for numbers

--- src/components/Number.vue
+++ src/components/Number.vue
@@ -1,16 +1,40 @@
 <template>
-  <v-card dark color="black" @click="click(num)">
-    <v-card-text>
-        {{ num }}
-    </v-card-text>
-	</v-card>
+<v-card dark color="black" @click="click(num)">
+  <v-card-text>
+      {{ num }}
+  </v-card-text>
+</v-card>
 </template>
 
 <script>
-  export default {
-    props: [
-			'num',
-			'click'
-		]
+const NUMBER_KEYCODES = {
+  48: 0,
+  49: 1,
+  50: 2,
+  51: 3,
+  52: 4,
+  53: 5,
+  54: 6,
+  55: 7,
+  56: 8,
+  57: 9
+};
+
+export default {
+  props: [
+    'num',
+    'click'
+  ],
+  created() {
+    document.addEventListener('keydown', (e) => {
+      const recognizedKeyCode = NUMBER_KEYCODES[e.keyCode];
+
+      if (recognizedKeyCode != this.num) {
+        return;
+      }
+
+      this.click(this.num);
+    });
   }
+}
 </script>
